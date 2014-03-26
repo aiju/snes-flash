3bf523f63319b21befbd90fb24a943be079ef57c
aiju <aiju@phicode.de>
2014-03-26 20:28:36 +0100

    simeple sd card controller

--- main.vhdl
+++ main.vhdl
@@ -4,17 +4,66 @@ use ieee.numeric_std.all;
 
 entity snes is
 	port(
-		clk : in std_logic;
-		sddat2 : out std_logic;
+		inclk : in std_logic;
+
 		snesdir : out std_logic;
-		cicin, cicclk, cicreset : in std_logic;
-		cicout : out std_logic
+		snesa : in unsigned(23 downto 0);
+		snesd : inout unsigned(7 downto 0);
+		snesrd, sneswr, snescart : in std_logic;
+		snesreset : in std_logic;
+
+		ramclk, ramcke, ramcs, ramwe : out std_logic;
+		ramcas, ramras, ramldqm, ramudqm : out std_logic;
+		rama : out unsigned(11 downto 0);
+		ramba : out unsigned(1 downto 0);
+		ramdq : inout unsigned(15 downto 0);
+
+		sdclk : out std_logic;
+		sdcmd : inout std_logic;
+		sddat : inout unsigned(3 downto 0);
+		sdcd : in std_logic
 	);
 end snes;
 
 architecture main of snes is
+	signal clk, reset, memmode, romen, memstart, memstep : std_logic;
+	signal snesrd0, snesrd1, sneswr0, sneswr1, snescart0 : std_logic;
+	signal snesa0 : unsigned(23 downto 0);
+	signal addr, sdaddr : unsigned(22 downto 0);
+	signal memdata, romdata, sddata, sdreg, snesd0 : unsigned(7 downto 0);
+	signal memen, cartrd, regen : std_logic;
+	signal romdata0 : std_logic_vector(7 downto 0);
+	
+	signal sden : std_logic;
 begin
-	snesdir <= '0';
-	sddat2 <= cicin;
-	cic0: entity work.cic port map(cicclk, cicreset, cicin, cicout);
+	process
+	begin
+		wait until rising_edge(clk);
+		snescart0 <= snescart;
+		snesrd0 <= snesrd;
+		sneswr0 <= sneswr;
+		sneswr1 <= sneswr0;
+		snesa0 <= snesa;
+		snesd0 <= snesd;
+	end process;
+
+	pll0: entity work.pll port map(inclk, clk, reset);
+	ramclk <= clk;
+	mem0: entity work.mem port map(clk, reset, ramcke, ramcs, ramwe, ramcas, ramras, ramldqm, ramudqm, rama, ramba, ramdq, memmode, memen, addr, memdata, memstart, memstep, sdaddr, sddata);
+	--rom0: entity work.bootrom port map(clk, addr(15 downto 0), romdata);
+	rom0: entity work.rom port map(std_logic_vector(addr(14 downto 0)), clk, (others => '0'), '0', romdata0);
+	romdata <= unsigned(romdata0);
+	regen <= sden;
+	cartrd <= snesrd0 nor (snescart0 and not regen);
+	memen <= (snesrd0 nor snescart0) when romen = '0' or addr(22 downto 16) /= "0000000" else '0';
+	snesd <= (others => 'Z') when cartrd = '0' else
+				sdreg when sden = '1' else
+	         memdata when memen = '1' else
+				romdata;
+	snesdir <= cartrd;
+	addr <= snesa0(23 downto 16) & snesa0(14 downto 0);
+	romen <= '0';
+
+	sden <= '1' when (snesa0 and X"40FFF0") = X"003000" else '0';
+	sd0: entity work.sd port map(clk, sdclk, not snesreset, sdcmd, sddat, sden and not snesrd0, sden and not sneswr0 and sneswr1, snesa0(3 downto 0), snesd0, sdreg);
 end main;
---
+++ sd.vhdl
@@ -0,0 +1,277 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sd is
+	port(
+		clk : in std_logic;
+		sdclk : out std_logic;
+		sdcd : in std_logic;
+		sdcmd : inout std_logic;
+		sddat : inout unsigned(3 downto 0);
+		regrd, regwr : in std_logic;
+		regaddr : in unsigned(3 downto 0);
+		regin: in unsigned(7 downto 0);
+		regout : out unsigned(7 downto 0)
+	);
+end sd;
+
+architecture main of sd is
+	signal div : unsigned(7 downto 0);
+	signal clkout, clkin : std_logic;
+	signal sdcd0, sdcd1, sdcmd0 : std_logic;
+	signal sddat0 : unsigned(3 downto 0);
+	signal cdctr : unsigned(23 downto 0);
+	signal card : std_logic := '0';
+	signal slow : std_logic := '1';
+	
+	type cmdstate_t is (IDLE, COMMAND, WAITRESP, RESP, RESP2, WAITBUSY, DONE, WAIT0);
+	signal cmdstate : cmdstate_t := IDLE;
+	signal buf : unsigned(47 downto 0);
+	type counter_t is range 0 to 255;
+	signal cmdctr, ctr : counter_t;
+	signal cmd : unsigned(5 downto 0);
+	constant NOOP : unsigned(5 downto 0) := "111111";
+	signal arg : unsigned(31 downto 0);
+	signal go, failed : std_logic;
+
+	signal response : unsigned(31 downto 0);
+	type state_t is (NOCARD, RESET, IDLE, ERROR);
+	signal state : state_t := NOCARD;
+	signal hc : std_logic;
+begin
+	process
+	begin
+		wait until rising_edge(clk);
+		div <= div + 1;
+		sdcmd0 <= sdcmd;
+		sdcd0 <= sdcd;
+		sdcd1 <= sdcd0;
+		sddat0 <= sddat;
+		if sdcd1 /= card then
+			cdctr <= X"000000";
+		else
+			cdctr <= cdctr + 1;
+			if cdctr = X"FFFFFF" then
+				card <= not sdcd1;
+			end if;
+		end if;
+	end process;
+	sdclk <= div(7) when slow = '1' else div(1);
+	clkin <= '1' when (slow = '1' and div = X"81") or (slow = '0' and div(1 downto 0) = "11") else '0';
+	clkout <= '1' when (slow = '1' and div = X"FF") or (slow = '0' and div(1 downto 0) = "11") else '0';
+
+	process
+		variable err : std_logic;
+		variable cond : boolean;
+	begin
+		wait until rising_edge(clk);
+		case cmdstate is
+		when IDLE =>
+			sdcmd <= '1';
+			sddat <= "ZZZZ";
+			if go = '1' then
+				buf(47) <= '0';
+				buf(46) <= '1';
+				buf(45 downto 40) <= cmd;
+				buf(39 downto 8) <= arg;
+				buf(7 downto 1) <= "0000000";
+				buf(0) <= '1';
+				cmdstate <= COMMAND;
+				cmdctr <= 0;
+				if cmd = NOOP then
+					cmdstate <= RESP2;
+				end if;
+			end if;
+		when COMMAND =>
+			if clkout = '1' then
+				buf <= buf(46 downto 1) & (buf(47) xor buf(7)) & '0';
+				if cmdctr < 40 then
+					sdcmd <= buf(47);
+					buf(1) <= buf(47) xor buf(7);
+					buf(4) <= buf(3) xor (buf(47) xor buf(7));
+				else
+					sdcmd <= buf(7);
+				end if;
+				if cmdctr = 47 then
+					sdcmd <= '1';
+				end if;
+				if cmdctr = 48 then
+					cmdctr <= 0;
+					case cmd is
+					when "000000" =>
+						sdcmd <= '1';
+						cmdstate <= WAIT0;
+					when others =>	
+						sdcmd <= 'Z';
+						cmdstate <= WAITRESP;
+					end case;
+				else
+					cmdctr <= cmdctr + 1;
+				end if;
+			end if;
+		when WAITRESP =>
+			if clkin = '1' and sdcmd0 = '0' then
+				if cmd = "000010" then
+					cmdstate <= RESP2;
+				else
+					cmdstate <= RESP;
+				end if;
+				buf(0) <= '0';
+			end if;
+		when RESP =>
+			if clkin = '1' then
+				buf <= buf(46 downto 0) & sdcmd0;
+				if cmdctr = 47 then
+					cmdstate <= DONE;
+					response <= buf(39 downto 8);
+					cond := false;
+					case cmd is
+					when "000010" | "101001" =>
+					when "000011" =>
+						cond := buf(23 downto 21) /= "000";
+					when "000111" =>
+						cmdstate <= WAITBUSY;
+					when "001000" =>
+						cond := buf(19 downto 8) /= arg(11 downto 0);
+					when others =>
+						cond := buf(39 downto 27) /= (12 downto 0 => '0');
+					end case;
+					if cond then
+						failed <= '1';
+					else
+						failed <= buf(46) or not buf(0);
+					end if;
+				else
+					cmdctr <= cmdctr + 1;
+				end if;
+			end if;
+		when RESP2 =>
+			if clkin = '1' then
+				if cmdctr = 135 then
+					cmdstate <= DONE;
+					response <= (others => '0');
+					failed <= '0';
+				else
+					cmdctr <= cmdctr + 1;
+				end if;
+			end if;
+		when WAIT0 =>
+			if cmdctr = 9 then
+				cmdstate <= DONE;
+				response <= (others => '0');
+				failed <= '0';
+			else
+				cmdctr <= cmdctr + 1;
+			end if;
+		when WAITBUSY =>
+			if clkin = '1' and sddat0(0) = '1' then
+				cmdstate <= DONE;
+			end if;
+		when DONE =>
+			cmdstate <= IDLE;
+		end case;
+		if card = '0' then
+			cmdstate <= IDLE;
+		end if;
+	end process;
+	
+	process
+	begin
+		wait until rising_edge(clk);
+		go <= '0';
+		case state is
+		when NOCARD =>
+			if card = '1' then
+				state <= RESET;
+				ctr <= 0;
+			end if;
+		when RESET =>
+			if cmdstate = IDLE then
+				go <= '1';
+				case ctr is
+				when 0 =>
+					cmd <= "000000";
+					arg <= (others => '0');
+				when 1 =>
+					cmd <= "001000";
+					arg <= X"000001A5";
+				when 2 =>
+					cmd <= "110111";
+					arg <= (others => '0');
+				when 3 =>
+					cmd <= "101001";
+					arg <= X"40700000";
+				when 4 =>
+					go <= '0';
+					if response(31) = '0' then
+						ctr <= 2;
+					else
+						ctr <= 5;
+						hc <= response(30);
+					end if;
+				when 5 =>
+					cmd <= "000010";
+					arg <= (others => '0');
+				when 6 =>
+					cmd <= "000011";
+				when 7 =>
+					cmd <= "000111";
+					arg(31 downto 16) <= response(31 downto 16);
+				when 8 => 
+					slow <= '0';
+					cmd <= NOOP;
+				when 9 =>
+					cmd <= "110111";
+				when 10 =>
+					cmd <= "101010";
+				when 11 =>
+					cmd <= "110111";
+				when 12 =>
+					cmd <= "000110";
+					arg(1) <= '1';
+				when others =>
+					cmd <= "000000";
+					go <= '0';
+					state <= IDLE;
+				end case;
+			end if;
+			if cmdstate = DONE then
+				if failed = '1' then
+					state <= ERROR;
+				else
+					ctr <= ctr + 1;
+				end if;
+			end if;
+		when IDLE =>
+		when ERROR =>
+		end case;
+		if card = '0' then
+			state <= NOCARD;
+			slow <= '1';
+		end if;
+	end process;
+	
+	process
+	begin
+		wait until rising_edge(clk);
+		if regrd = '1' then
+			regout <= X"00";
+			case to_integer(regaddr) is
+			when 0 =>
+				if state /= IDLE then
+					regout(7) <= card;
+				end if;
+				regout(6) <= not card or failed;
+				if card = '1' then
+					regout(5 downto 0) <= cmd;
+				end if;
+			when 1 => regout <= response(7 downto 0);
+			when 2 => regout <= response(15 downto 8);
+			when 3 => regout <= response(23 downto 16);
+			when 4 => regout <= response(31 downto 24);
+			when others =>
+			end case;
+		end if;
+	end process;
+end main;
