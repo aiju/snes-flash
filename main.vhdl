67b11b38c2a4a54b0608190479ff3a438150041c
aiju <aiju@phicode.de>
2014-03-29 22:16:50 +0100

    improved dram refresh code

--- main.vhdl
+++ main.vhdl
@@ -10,7 +10,7 @@ entity snes is
 		snesa : in unsigned(23 downto 0);
 		snesd : inout unsigned(7 downto 0);
 		snesrd, sneswr, snescart : in std_logic;
-		snesreset : in std_logic;
+		snesreset, snesclk : in std_logic;
 
 		ramclk, ramcke, ramcs, ramwe : out std_logic;
 		ramcas, ramras, ramldqm, ramudqm : out std_logic;
@@ -26,8 +26,8 @@ entity snes is
 end snes;
 
 architecture main of snes is
-	signal clk, reset, memmode, romen, memstart, txstart, txstep : std_logic;
-	signal snesrd0, snesrd1, sneswr0, sneswr1, snescart0, snesreset0 : std_logic;
+	signal clk, reset, refresh, memmode, romen, memstart, txstart, txstep : std_logic;
+	signal snesrd0, snesrd1, sneswr0, sneswr1, snescart0, snesreset0, snesclk0 : std_logic;
 	signal snesa0 : unsigned(23 downto 0);
 	signal romaddr, dmaaddr : unsigned(22 downto 0);
 	signal memdata, romdata, txdata, sdreg, dmareg, snesd0 : unsigned(7 downto 0);
@@ -35,6 +35,10 @@ architecture main of snes is
 	signal romdata0 : std_logic_vector(7 downto 0);
 	
 	signal sden : std_logic;
+	
+	type refcnt_t is range 0 to 63;
+	constant thresh : refcnt_t := 30;
+	signal rctr : refcnt_t;
 begin
 	process
 	begin
@@ -46,11 +50,23 @@ begin
 		sneswr1 <= sneswr0;
 		snesa0 <= snesa;
 		snesd0 <= snesd;
+		snesclk0 <= snesclk;
+		if snesclk0 = '0' then
+			if rctr = thresh then
+				refresh <= '1';
+			else
+				refresh <= '0';
+				rctr <= rctr + 1;
+			end if;
+		else
+			refresh <= '0';
+			rctr <= 0;
+		end if;
 	end process;
 
 	pll0: entity work.pll port map(inclk, clk, reset);
 	ramclk <= clk;
-	mem0: entity work.mem port map(clk, reset, ramcke, ramcs, ramwe, ramcas, ramras, ramldqm, ramudqm, rama, ramba, ramdq, memmode, memen, romaddr, memdata, memstart, txstep, dmaaddr, txdata);
+	mem0: entity work.mem port map(clk, reset, ramcke, ramcs, ramwe, ramcas, ramras, ramldqm, ramudqm, rama, ramba, ramdq, memmode, memen, romaddr, memdata, memstart, txstep, dmaaddr, txdata, refresh);
 	--rom0: entity work.bootrom port map(clk, addr(15 downto 0), romdata);
 	rom0: entity work.rom port map(std_logic_vector(romaddr(14 downto 0)), clk, (others => '0'), '0', romdata0);
 	romdata <= unsigned(romdata0);
--- mem.vhdl
+++ mem.vhdl
@@ -14,22 +14,31 @@ entity mem is
 		data0 : out unsigned(7 downto 0);
 		start, step : in std_logic;
 		addr1 : in unsigned(22 downto 0);
-		data1 : in unsigned(7 downto 0)
+		data1 : in unsigned(7 downto 0);
+		refresh : in std_logic
 	);
 end mem;
 
 architecture main of mem is
-	type state_t is (RESETCMD, IDLE, READCMD, WRITECMD);
+	type state_t is (RESETCMD, IDLE, READCMD, WRITECMD, AUTOREFRESH);
 	signal curaddr : unsigned(22 downto 0);
 	signal buf : unsigned(7 downto 0);
 	signal state : state_t := RESETCMD;
-	signal ctr : unsigned(2 downto 0) := "000";
+	type ctr_t is range 0 to 7;
+	constant tRCD : ctr_t := 2;
+	constant tCAS : ctr_t := 3;
+	constant tRP : ctr_t := 1;
+	constant tRC : ctr_t := 5;
+	signal ctr : ctr_t;
+	signal stepped : std_logic;
 begin
 	data0 <= buf;
+	cke <= '1';
 	process
+		variable sel : boolean;
+		variable addr : unsigned(22 downto 0);
 	begin
 		wait until rising_edge(clk);
-		cke <= '1';
 		cs <= '0';
 		we <= '1';
 		cas <= '1';
@@ -37,77 +46,83 @@ begin
 		a <= (others => '0');
 		ba <= (others => '0');
 		dq <= (others => 'Z');
+		if mode = '1' and start = '1' and step = '1' then
+			stepped <= '1';
+		end if;
 		case state is
 		when RESETCMD =>
 			ctr <= ctr + 1;
 			case ctr is
-			when "000" =>
+			when 0 =>
 				ras <= '0';
 				we <= '0';
 				a(10) <= '1';
-			when "001" | "011" =>
+			when 1 | 3 =>
 				ras <= '0';
 				cas <= '0';
-			when "101" =>
+			when 5 =>
 				ras <= '0';
 				cas <= '0';
 				we <= '0';
 				a(5) <= '1';
-			when "110" =>
-				ctr <= "000";
+			when 6 =>
 				state <= IDLE;
 			when others =>
 			end case;
 		when IDLE =>
+			sel := false;
 			cs <= '1';
-			cke <= '0';
-			if mode = '0' and en = '1' and addr0 /= curaddr then
-				cke <= '1';
+			if refresh = '1' and (mode = '0' or start = '0') then
+				ctr <= 0;
 				cs <= '0';
-				ctr <= "000";
+				ras <= '0';
+				cas <= '0';
+				state <= AUTOREFRESH;
+			end if;
+			if mode = '0' and en = '1' and addr0 /= curaddr then
+				sel := true;
 				state <= READCMD;
-				curaddr <= addr0;
+				addr := addr0;
 			end if;
 			if mode = '1' and start = '1' then
-				cke <= '1';
-				cs <= '0';
-				ctr <= "000";
+				sel := true;
 				state <= WRITECMD;
-				curaddr <= addr1;
+				addr := addr1;
+			end if;
+			if sel then
+				cs <= '0';
+				ras <= '0';
+				cas <= '1';
+				curaddr <= addr;
+				ba <= addr(22 downto 21);
+				a <= addr(20 downto 9);
+				ldqm <= addr(0);
+				udqm <= not addr(0);
+				ctr <= 0;
 			end if;
 		when READCMD =>
 			ctr <= ctr + 1;
 			case ctr is
-			when "001" =>
-				ras <= '0';
-				ba <= curaddr(22 downto 21);
-				a <= curaddr(20 downto 9);
-				ldqm <= curaddr(0);
-				udqm <= not curaddr(0);
-			when "100" =>
+			when tRCD =>
 				cas <= '0';
 				ba <= curaddr(22 downto 21);
 				a(7 downto 0) <= curaddr(8 downto 1);
 				a(10) <= '1';
-			when "111" =>
+			when tRCD+tCAS =>
 				if curaddr(0) = '1' then
 					buf <= dq(15 downto 8);
 				else
 					buf <= dq(7 downto 0);
 				end if;
-				ctr <= "000";
 				state <= IDLE;
 			when others =>
 			end case;
 		when WRITECMD =>
 			ctr <= ctr + 1;
 			case ctr is
-			when "001" =>
-				ras <= '0';
-				ba <= curaddr(22 downto 21);
-				a <= curaddr(20 downto 9);
-			when "100" =>
-				if step = '1' then
+			when tRCD =>
+				if step = '1' or stepped = '1' then
+					stepped <= '0';
 					cas <= '0';
 					we <= '0';
 					ba <= curaddr(22 downto 21);
@@ -122,22 +137,26 @@ begin
 					curaddr <= curaddr + 1;
 				end if;
 				if start = '1' then
-					ctr <= "100";
+					ctr <= tRCD;
 				end if;
-			when "110" =>
+			when tRCD+1 =>
 				ras <= '0';
 				we <= '0';
 				a(10) <= '1';
-			when "111" =>
-				ctr <= "000";
+			when tRCD+1+tRP =>
 				state <= IDLE;
 			when others =>
 			end case;
+		when AUTOREFRESH =>
+			ctr <= ctr + 1;
+			if ctr = tRC then
+				state <= IDLE;
+			end if;
 		end case;
 		if reset = '0' then
 			state <= RESETCMD;
-			ctr <= "000";
-			cke <= '0';
+			ctr <= 0;
+			cs <= '1';
 		end if;
 	end process;
 end main;
